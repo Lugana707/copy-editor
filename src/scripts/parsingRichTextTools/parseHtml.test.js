c325063b0fea6cebab921c9507051e6ac0800d49
Lugana707 <sam.m.albon@outlook.com>
2021-09-15 17:02:59 -0400

    Added more complex smoke test for parseHtml

--- src/scripts/parsingRichTextTools/parseHtml.test.js
+++ src/scripts/parsingRichTextTools/parseHtml.test.js
@@ -30,12 +30,64 @@ describe('parseHtml', () => {
       { style: 'bold-italic', html: `<div><i><b>${content}</b></i></div>` },
       { style: 'bold-italic', html: `<div><b><i>${content}</i></b></div>` },
       { style: 'normal', html: `<div>${content}</div>` },
-    ].map(({ style, html }) => {
+      { style: 'normal', html: `<div><span style="">${content}</span></div>` },
+    ].forEach(({ style, html }) => {
       describe(`with ${style} nodes`, () => {
-        it(`returns array with ${style} nodes`, () => {
-          expect(parseHtml(html)).toEqual(getTestResults({ style }));
-        });
+        it(`returns array with ${style} nodes`, () => expect(parseHtml(html)).toEqual(getTestResults({ style })));
       });
     });
+  });
+
+  describe('with multiple lines and styles', () => {
+    const content = `
+      <div>
+        <i>Hello&nbsp;</i>
+        <b>World</b>
+        !
+      </div>
+      <div>
+        <b>Hello</b>
+        <b><i>&nbsp;World</i></b>
+        !
+      </div>
+    `
+      .trim()
+      .replace('\n', '')
+      .replace(/\s/g, '');
+
+    it('returns array with correctly styled nodes', () => expect(parseHtml(content)).toEqual([
+      {
+        content: [
+          {
+            style: 'italic',
+            content: 'Hello\xa0',
+          },
+          {
+            style: 'bold',
+            content: 'World',
+          },
+          {
+            style: 'normal',
+            content: '!',
+          },
+        ],
+      },
+      {
+        content: [
+          {
+            style: 'bold',
+            content: 'Hello',
+          },
+          {
+            style: 'bold-italic',
+            content: '\xa0World',
+          },
+          {
+            style: 'normal',
+            content: '!',
+          },
+        ],
+      },
+    ]));
   });
 });
